8f1d13a1a58c8dfcf254d1a926b3a1f7eac3fd61
Okoro Emmanuel <emmanuelokoro273@gmail.com>
2026-03-22 22:43:33 +0100

    Adjust height of footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer: React.JSX.Element = (
-  <footer className="sm:h-max md:row-start-3 md:row-end-4 h-[15vh] bg-gray-800 text-white p-4 text-center text-sm md:col-start-1 md:col-end-2">
+  <footer className="sm:h-full md:row-start-3 md:row-end-4 h-[15vh] bg-gray-800 text-white p-4 text-center text-sm md:col-start-1 md:col-end-2">
     &copy; {new Date().getFullYear()} Country Quiz Master. All rights reserved.
     <br />
     Created by Okoro Emmanuel.
